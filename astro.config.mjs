7fb562d2ca00b1508dfcb9d9d46af8a0ebf496e3
KADRDulmin <115799307+KADRDulmin@users.noreply.github.com>
2025-02-20 22:29:38 +0530

    changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,8 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
 	site: 'https://kadrdulmin.github.io',
-	base: '/raminda-kariyawasam',
+	base: 'raminda-kariyawasam',
 	integrations: [mdx(), sitemap()],
-	output: 'static'
 });
