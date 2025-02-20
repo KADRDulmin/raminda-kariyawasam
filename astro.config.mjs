1580e6ed909aadd112a02fa19fca58991b7f096c
KADRDulmin <115799307+KADRDulmin@users.noreply.github.com>
2025-02-20 22:19:33 +0530

    Setup GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://kadrdulmin.github.io',
+	base: '/raminda-kariyawasam',
 	integrations: [mdx(), sitemap()],
+	output: 'static'
 });
