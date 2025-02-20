008443a29faf0f3f3b98f25b062d081c5d77edf2
KADRDulmin <115799307+KADRDulmin@users.noreply.github.com>
2025-02-20 14:57:22 +0530

    update projects

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Raminda Kariyawasam";
+export const SITE_DESCRIPTION =
+    "Software Engineer Portfolio - Raminda Kariyawasam";
